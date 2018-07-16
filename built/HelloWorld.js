76daebb9a4308e44c7889e188f070aeeac1e98c4
Robert Ogiba <robert.ogiba@outlook.com>
2018-07-16 19:24:44 +0200

    ReEnable debugging in project

---
+++ built/HelloWorld.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Person_1 = require("./model/Person");
+class Startup {
+    static main() {
+        console.log('Hello World!');
+        var person = new Person_1.Person("Bobo", "Ogibon");
+        console.log(person.fullName);
+        return 0;
+    }
+}
+Startup.main();
+//# sourceMappingURL=HelloWorld.js.map
---
+++ built/model/Person.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Person {
+    constructor(firstName, lastName) {
+        this.fullName = firstName + " " + lastName;
+    }
+}
+exports.Person = Person;
+//# sourceMappingURL=Person.js.map
